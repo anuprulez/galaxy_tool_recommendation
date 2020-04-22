8f4a28da8e4337f67ab9420a7327afa637dd92bc
Anup Kumar <anup.rulez@gmail.com>
2020-04-22 12:42:07 +0200

    Add ipy

--- train.sh
+++ train.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python scripts/main.py -wf data/workflow-connections-19-09.tsv -tu data/tool-popularity-19-09.tsv -om data/tool_recommendation_model.hdf5 -cd '2017-12-01' -pl 25
+python scripts/main.py -wf data/workflow-connections-19-09.tsv -tu data/tool-popularity-19-09.tsv -om data/tool_recommendation_model_statistical_model.hdf5 -cd '2017-12-01' -pl 25
