7f41e4ae016058a392fcb51fdac99b39735de72e
Anup Kumar <anup.rulez@gmail.com>
2020-08-17 19:04:20 +0530

    Add sample tests

---
+++ tests/test_extract_workflow_connections.py
@@ -0,0 +1,12 @@
+import unittest
+
+
+class TestWorkflows(unittest.TestCase):
+    """ """
+
+    def test_all(self):
+        print("Test passed")
+
+
+if __name__ == "__main__":
+    unittest.main()
