24d51bc020ddc554a821d0d2bf22207e8cbbd073
Anup Kumar <anup.rulez@gmail.com>
2020-08-18 16:42:15 +0530

    Update test

--- scripts/tests/test_extract_workflow_connections.py
+++ scripts/tests/test_extract_workflow_connections.py
@@ -3,11 +3,17 @@ import unittest
 from scripts import utils
 from scripts import extract_workflow_connections
 
-class TestWorkflows(unittest.TestCase):
-    """ """
+class TestExtractWorkflowConnections(unittest.TestCase):
 
-    def test_all(self):
-        print("Test passed")
+    def __init__(self):
+        self.workflow_file_path = "scripts/tests/test_data/worflow-connection-subset-20-04_test.tsv"
+        self.extract_wf = extract_workflow_connections.ExtractWorkflowConnections()
+
+    def test_read_tabular_file(self):
+        unique_paths, compatible_next_tools, standard_connections = self.extract_wf.read_tabular_file(self.workflow_file_path)
+        assert len(unique_paths) > 0
+        assert len(compatible_next_tools) > 0
+        assert len(standard_connections) > 0
 
 
 if __name__ == "__main__":
