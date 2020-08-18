7e8f5da48d7d31293f078e5baebc441b693aafc0
Anup Kumar <anup.rulez@gmail.com>
2020-08-18 16:49:07 +0530

    Update test

--- scripts/tests/test_extract_workflow_connections.py
+++ scripts/tests/test_extract_workflow_connections.py
@@ -5,12 +5,10 @@ from scripts import extract_workflow_connections
 
 class TestExtractWorkflowConnections(unittest.TestCase):
 
-    def __init__(self):
-        self.workflow_file_path = "scripts/tests/test_data/worflow-connection-subset-20-04_test.tsv"
-        self.extract_wf = extract_workflow_connections.ExtractWorkflowConnections()
-
     def test_read_tabular_file(self):
-        unique_paths, compatible_next_tools, standard_connections = self.extract_wf.read_tabular_file(self.workflow_file_path)
+        workflow_file_path = "scripts/tests/test_data/worflow-connection-subset-20-04_test.tsv"
+        extract_wf = extract_workflow_connections.ExtractWorkflowConnections()
+        unique_paths, compatible_next_tools, standard_connections = extract_wf.read_tabular_file(workflow_file_path)
         assert len(unique_paths) > 0
         assert len(compatible_next_tools) > 0
         assert len(standard_connections) > 0
