709e6f97ee479b7db7c1268dda598a250b76db69
Anup Kumar <anup.rulez@gmail.com>
2022-04-14 15:29:18 +0200

    adjust to new version of h5py

--- scripts/utils.py
+++ scripts/utils.py
@@ -72,14 +72,6 @@ def remove_file(file_path):
     if os.path.exists(file_path):
         os.remove(file_path)
 
-'''
-def get_weighted_loss(weights):
-    def weighted_loss(y_true, y_pred):
-        return K.mean((weights[:,0]**(1-y_true))*(weights[:,1]**(y_true))*K.binary_crossentropy(y_true, y_pred), axis=-1)
-    return weighted_loss
-
-'''
-
 
 def weighted_loss(class_weights, batch_size):
     """
